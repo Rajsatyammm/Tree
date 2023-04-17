ede003067c300e64baaa91ccc72edc9b2f6aacab
Satyam Raj <rajsatyammm@gmail.com>
2023-04-17 19:55:28 +0530

    Create Build Binary Tree.java

---
+++ Build Binary Tree.java
@@ -0,0 +1,38 @@
+public class BuildPreorderTree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+
+        static int idx = -1;
+
+        public static Node buildTree(int[] nodes) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+      public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1, 7, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+       
+
+    }
+
+}
