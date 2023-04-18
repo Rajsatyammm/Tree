73519bdcb2f36b138ea66856b9551cd23078d528
Satyam Raj <rajsatyammm@gmail.com>
2023-04-18 20:37:10 +0530

    Create Level Order Traversing.java

---
+++ Level Order Traversing.java
@@ -0,0 +1,26 @@
+static Queue<Node> q = new LinkedList<>();
+
+        public static void levelOrder(Node root) {
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node curr = q.remove();
+                if (curr == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(curr.data + " ");
+                    if (curr.left != null) {
+                        q.add(curr.left);
+                    }
+                    if (curr.right != null) {
+                        q.add(curr.right);
+                    }
+                }
+            }
+        }
