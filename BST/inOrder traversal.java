41b4e7d7aeb07a71979f3a353bc56b0f1199642f
Satyam Raj <rajsatyammm@gmail.com>
2023-04-19 22:59:21 +0530

    Create inOrder traversal.java

---
+++ BST/inOrder traversal.java
@@ -0,0 +1,9 @@
+public void inOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
